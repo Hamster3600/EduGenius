c498d29b1d0ad86cc807f885591aff1a278b7ce9
Denis Pylypenko <den.pylypen@gmail.com>
2025-11-28 12:49:10 +0100

    poprawa installera jeszcze raz

--- installer.sh
+++ installer.sh
@@ -45,7 +45,7 @@ fi
 #pobieranie bibliotek
 echo -e "${YELLOW}Instaluję biblioteki pythona...${NC}"
 pip3 install --upgrade pip --break-system-packages 
-pip3 install --break-system-packages  --allow-all-external \
+pip3 install --break-system-packages \
     customtkinter \
     pypdf \
     python-docx \
